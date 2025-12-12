513bdef6aa233cf1981227c74625d2f0eda9af45
Mohammed <mohammedbabatunde2001@gmail.com>
2025-12-12 09:05:48 +0100

    worked on some pipelines

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,6 +6,5 @@ import yfinance as yf
 def get_data(ticker: str = "SPY", start: str = "1993-01-01", end: str = "2025-12-11"):
 	df = yf.download(ticker, start=start, end=end)
 	df.to_csv(f"../data/raw/{ticker}.csv")
+	return df
 
-
-get_data()
